3d24e5d62f5f5c1b0a855850773946834f94f8ed
ungcas000 <cassandraung@Cassandras-MacBook-Pro.local>
2015-10-25 02:19:50 -0700

    transactions added

--- new_wallet.js
+++ new_wallet.js
@@ -1,7 +1,7 @@
 //Read email from Facebook
 var emailRef = new Firebase("");
 
-//Creates new user wallet
+//Creates new user wallet and retrieves user key
 function newWallet(){
   var request = new XMLHttpRequest();
 
@@ -21,21 +21,41 @@ function newWallet(){
   api_code= \
   email= ";
 
-  request.send(body);
+  var walletDict = request.send(body);
+  return walletDict["guid"];
 
-//Writes email and id from new wallet into database
-var gambitRef = new Firebase("https://gambitapp.firebaseio.com//");
+//checks to see if user is in database
+var isNewUser = true;
 
-gambitRef.set({
-    email:
-    id:
+var userRef = new Firebase("https://gambit-users.firebaseio.com/"));
+ref.onAuth(function(authData) {
+  if (authData && isNewUser) {
+    // save the user's profile into the database so we can list users,
+    userRef.child("users").child(authData.uid).set({
+      provider: authData.provider,
+      name: getName(authData)
+      email:
+      password:
+      guid: newWallet(),
+    });
+  }
 });
+
+// find a suitable name based on the meta info given by each provider
+function getName(authData) {
+  switch(authData.provider) {
+     case 'password':
+       return authData.password.email.replace(/@.*/, '');
+     case 'facebook':
+       return authData.facebook.displayName;
+  }
+}
 
 //retrieves total balance from users account
 function balanceTotal(){
   var request = new XMLHttpRequest();
 
-  request.open('POST', 'https://blockchain.info/merchant/(key)/balance');
+  request.open('POST', 'https://blockchain.info/merchant/(guid)/balance');
 
   request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
@@ -53,5 +73,34 @@ function balanceTotal(){
   return totalDict["balance"];
 };
 
-if (balanceTotal() >= .0035 + .0035(.02)){
-  return
+//voter's payment
+function userPayment (){
+  var request = new XMLHttpRequest();
+
+  request.open('POST', 'https://blockchain.info/merchant/(guid)/payment');
+
+  request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+  request.onreadystatechange = function () {
+    if (this.readyState === 4) {
+      console.log('Status:', this.status);
+      console.log('Headers:', this.getAllResponseHeaders());
+      console.log('Body:', this.responseText);
+    }
+  };
+
+  var body = "password=(currentuserpassword) \
+  address=& \
+  amount= 350000 + 350000(.01)& \
+  from=& \
+  fee=10000&";
+
+  request.send(body);
+};
+
+//checks to see if user has sufficient balance
+if (balanceTotal() >= 360000 + 350000(.01)){
+  return userPayment();
+} else {
+  return "Transaction Error";
+}
